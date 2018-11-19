30b4eb347d119ada6a852fe105bf53f3a2c50ae1
eprigmore <eprigmore1@gmail.com>
2018-11-19 11:22:18 -0700

    ChaptersViewController correctly displaying. If only 1 chapter, book view goes to scripture view, else goes to chapter view. Chapter view goes to correct chapter.

--- Project 3 Menden Emily/Controller/BooksViewController.swift
+++ Project 3 Menden Emily/Controller/BooksViewController.swift
@@ -13,7 +13,10 @@ class BooksViewController: UITableViewController {
     private struct Storyboard {
         static let BookCellIdentifier = "BookCell"
         static let ShowScripturesSegueIdentifier = "ShowChapterContent"
+        static let ShowChapterListSegueIdentifier = "ShowChapterList"
     }
+    
+    // MARK:- Properties
     
     var books = [Book]()
     var volume = ""
@@ -29,16 +32,24 @@ class BooksViewController: UITableViewController {
     
     // MARK:- Segues
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let scripturesVC = segue.destination as? ScripturesViewController {
-            if let indexPath = sender as? IndexPath {   // could send a Book instead
-                scripturesVC.bookId = books[indexPath.row].id
-                scripturesVC.chapter = 2
+        
+        if let book = sender as? Book {
+            if let scripturesVC = segue.destination as? ScripturesViewController {
+                /*if let indexPath = sender as? IndexPath {   // could send a Book instead
+                 scripturesVC.bookId = books[indexPath.row].id
+                 scripturesVC.chapter = 2
+                 }*/
+                scripturesVC.bookId = book.id
+                scripturesVC.chapter = 0
+            }
+            if let chaptersVC = segue.destination as? ChaptersViewController {
+                chaptersVC.book = book
             }
         }
-        // when preparing for segue to map, could use Configuration object or could set all values needed here
     }
     
     // MARK:- Table view Data source
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return books.count
     }
@@ -52,9 +63,20 @@ class BooksViewController: UITableViewController {
     }
     
     // MARK:- Table view delegate
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //performSegue(withIdentifier: Storyboard.ShowScripturesSegueIdentifier, sender: self)
-        performSegue(withIdentifier: Storyboard.ShowScripturesSegueIdentifier, sender: indexPath)
+        let bookId = books[indexPath.row].id
+        let book = GeoDatabase.sharedGeoDatabase.bookForId(bookId)
+        
+        if let numChapters = book.numChapters {
+            if numChapters > 1 {
+                performSegue(withIdentifier: Storyboard.ShowChapterListSegueIdentifier, sender: book)
+            } else {
+                performSegue(withIdentifier: Storyboard.ShowScripturesSegueIdentifier, sender: book)
+            }
+        } else {
+            performSegue(withIdentifier: Storyboard.ShowScripturesSegueIdentifier, sender: book)
+        }
     }
     
     // MARK:- Helpers
---
+++ Project 3 Menden Emily/Controller/ChaptersViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ChaptersViewController.swift
+//  Project 3 Menden Emily
+//
+//  Created by Emily Prigmore on 11/19/18.
+//  Copyright © 2018 IS 543. All rights reserved.
+//
+
+import UIKit
+
+class ChaptersViewController : UITableViewController {
+    
+    private struct Storyboard {
+        static let ChapterCellIdentifier = "ChapterCell"
+        static let ShowChapterContentSegueIdentifier = "ShowChapterContent"
+    }
+    
+    // MARK:- Properties
+
+    var book = Book()
+    
+    // MARK:- Segues
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == Storyboard.ShowChapterContentSegueIdentifier {
+            if let scripturesVC = segue.destination as? ScripturesViewController {
+                scripturesVC.bookId = book.id
+                
+                if let indexPath = sender as? IndexPath {
+                    scripturesVC.chapter = indexPath.row + 1    // chapters indexed by 1, rows indexed by 0
+                }
+            }
+        }
+    }
+    
+    // MARK:- Table view data source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return book.numChapters ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:Storyboard.ChapterCellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = "Chapter \(indexPath.row + 1)"
+        
+        return cell
+    }
+    
+    // MARK:- Table view delegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: Storyboard.ShowChapterContentSegueIdentifier, sender: indexPath)
+    }
+}
